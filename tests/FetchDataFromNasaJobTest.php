00f5a7438c5315abbc5c32f3bfd2d900941ff0a8
ironman126 <rishi.iyer09@yahoo.com>
2019-10-19 14:45:31 +0100

    Add tests

--- tests/FetchDataFromNasaJobTest.php
+++ tests/FetchDataFromNasaJobTest.php
@@ -3,6 +3,39 @@
 namespace Tests;
 
 class FetchDataFromNasaJobTest extends TestCase
-{
-    // public function 
-}
+    {
+
+    }
+    public function TestIncorporatingMultipleFiles()
+    {
+
+    }
+
+    public function TestHasDataChanged()
+    {
+
+    }
+
+
+    public function Testisunavailable()
+
+    {
+
+    }
+
+    public function TestprocessData()
+    {
+
+    }
+
+    public function Testgetdata()
+    {
+
+    }
+
+    public function Testcheckhead()
+    {
+
+    }
+
+   
