254838befe69a0e16701476b88cddcc1577d8544
Aarush Thumma <Aarush.Thumma@gmail.com>
2019-10-20 10:17:20 +0100

    Added comments

--- tests/NasaClientTest.php
+++ tests/NasaClientTest.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace Tests;
-
+//importing libraries
 use Mockery\Mockery;
 use App\Nasa\Client as NasaClient;
 use GuzzleHttp\Client as HttpClient;
 use App\Jobs\FetchDataFromNasaJob;
 use Psr\Http\Message\ResponseInterface;
 
+//tests getting NASA data
 class NasaClientTest extends TestCase
 {
+    //main function
     public function TestGetData()
     {
         $http_client = Mockery::mock(HttpClient::class);
